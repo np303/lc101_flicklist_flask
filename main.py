d64e7c12726c12a848ced93da546eb208fda96a1
np303 <2ep303@gmail.com>
2019-04-01 20:03:39 -0500

    finished random movies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -10,20 +11,26 @@ def index():
     movie = get_random_movie()
 
     # build the response string
-    content = "<h1>Movie of the Day</h1>"
-    content += "<ul>"
-    content += "<li>" + movie + "</li>"
-    content += "</ul>"
+    day1 = "<h1>Movie of the Day</h1>"
+    day1 += "<ul>"
+    day1 += "<li>" + movie[0] + "</li>"
+    day1 += "</ul>"
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
-
-    return content
+    day2 = "<h1>Tomorrow's Movie</h1>"
+    day2 += "<ul>"
+    day2 += "<li>" + movie[1] + "</li>"
+    day2 += "</ul>"
+    
+    return day1 + day2
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movies = ["The Crow", "Star Wars", "Fast N Furious", "Heavy Metal"]
+    random.shuffle(movies)
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    return (movies[1], movies[2])
 
 
 app.run()
